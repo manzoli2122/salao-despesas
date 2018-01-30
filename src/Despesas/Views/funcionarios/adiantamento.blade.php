bbffbbe2109da76f8507988939015c6a1baceeaa
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-01-30 21:14:51 -0200

    30-01-2018

--- src/Despesas/Views/funcionarios/adiantamento.blade.php
+++ src/Despesas/Views/funcionarios/adiantamento.blade.php
@@ -27,27 +27,26 @@
     <section class="row text-center placeholders">
         <div class="col-2 col-sm-2 placeholder"></div>
         <div class="col-8 col-sm-8 placeholder">
-        
-             {!! Form::open(['route' => 'adiantamento.createAdiantamento', 'class' => 'form form-search form-ds'])!!}
+                <form method="POST" action="{{route('adiantamento.createAdiantamento')}}" accept-charset="UTF-8" class="form form-search form-ds">
+                    {{csrf_field()}}
+               
+                    <input name="funcionario_id" value="{{$funcionario->id}}" type="hidden">
+                    <input name="tipo" value="adiantamento" type="hidden">
 
-                {{ Form::hidden('funcionario_id',  $funcionario->id ) }}
-                {{ Form::hidden('tipo', 'adiantamento' ) }}
-				<div class="form-group">
-                    <label for="descricao" class="col-form-label">Descrição:</label>
-                    {!! Form::text('descricao' , null , ['placeholder' => 'Descrição', 'class' => 'form-control'])!!}
-				</div>
-				<div class="form-group">
-                    <label for="valor" class="col-form-label">Valor:</label>
-                    {!! Form::number('valor', null, ['placeholder' => 'Valor', 'step' => '0.01', 'class' => 'form-control']) !!}
-                </div>
-				
-				<div class="form-group">
-                    {!! Form::submit('Enviar' , ['class' => 'btn btn-success']) !!}
-				</div>
-            {!! Form::close()!!}
-        </div>   
+                    <div class="form-group">
+                        <label for="descricao" class="col-form-label">Descrição:</label>
+                        <input placeholder="Descrição" class="form-control" name="descricao" type="text"> 
+                    </div>
+                    <div class="form-group">
+                        <label for="valor" class="col-form-label">Valor:</label>
+                        <input placeholder="Valor" step="0.01" class="form-control" max="22.5" name="valor" type="number">
+                    </div>
+                        
+                    <div class="form-group">
+                        <input class="btn btn-warning" value="Enviar" type="submit">
+                    </div>                             
+                </form>
+            </div>   
         <div class="col-2 col-sm-2 placeholder"></div>     
     </section>
-
-
 @endsection
